9510330f8c9ea449abff0d15373c6c1da8ef40ea
wuhui <xiaohui_hubei@163.com>
2014-07-14 20:27:27 +0800

    add chinese language UI

---
+++ zh.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en_AS">
+<context>
+    <name>CBaiduTranslater</name>
+    <message>
+        <location filename="cbaidutranslater.cpp" line="150"/>
+        <source>error_msg</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="cbaidutranslater.cpp" line="159"/>
+        <source>trans_result</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="23"/>
+        <source>English</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="30"/>
+        <source>Chinese</source>
+        <translation>中文</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="76"/>
+        <location filename="mainwindow.ui" line="83"/>
+        <source>Translate</source>
+        <translation>翻译</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="13"/>
+        <source>MyTranslater developed with Qt</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
