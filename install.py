4106ae7e6f2f5e13760f88fa7a8f9b5eedf2844f
wuhui <xiaohui_hubei@163.com>
2014-07-20 17:18:25 +0800

    add install file

--- install.py
+++ install.py
@@ -7,17 +7,21 @@ if __name__ == "__main__":
     dir1 = "style"
     dir2 = "language"
 
-    srcDir = "."
+    srcDir = os.path.abspath('.') 
     dstDir = input("Enter install directory path:")
-    if (os.path.exists(dstDir)):
+    if os.path.exists(dstDir):
         print("Destination directory has existed.")
     else:
         os.mkdir(dstDir)
+        dstDir = os.path.abspath(str(dstDir))
+        print("srcDir=", srcDir)
+        print("dstDir=", dstDir)
         try:
-            shutil.copy2(srcDir + "/" + file1, dstDir + "/" + file1)
-            shutil.copytree(srcDir + "/" + dir1, dstDir + "/" + dir1)
-            shutil.copytree(srcDir + "/" + dir2, dstDir + "/" + dir2)
+            shutil.copy2(srcDir + '/' + file1, dstDir + '/' + file1)
+            shutil.copytree(srcDir + '/' + dir1, dstDir + '/' + dir1)
+            shutil.copytree(srcDir + '/' + dir2, dstDir + '/' + dir2)
         except Error as e:
             print(str(e))
         else:
             print("finish copying")
+        os.symlink(dstDir + '/' + file1, os.path.expanduser("~") + '/Desktop/' + file1)
